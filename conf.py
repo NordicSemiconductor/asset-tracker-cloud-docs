a7051d74a20c7a5075d565d4b4ce686c2456e915
Markus Tacker <Markus.Tacker@NordicSemi.no>
2021-02-09 15:23:39 +0100

    build: remove reference to non-existing folder

--- conf.py
+++ conf.py
@@ -58,11 +58,6 @@ html_context = {
     'github_version': 'saga/'
 }
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
-
 html_logo = 'docs/logo-with-text-white.svg'
 
 master_doc = 'README'
