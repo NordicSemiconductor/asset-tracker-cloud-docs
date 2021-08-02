f9fbcc3ce91285ca3ed8064d98458084a86fb9e8
Lena Haraldseid <lena.haraldseid@nordicsemi.no>
2021-08-02 14:26:21 +0200

    fix: update system overview diagram to follow Nordic styleguide

--- scripts/dot2svg.sh
+++ scripts/dot2svg.sh
@@ -13,6 +13,6 @@ echo $1
 echo $2
 echo ${OUT}
 
-dot -Tsvg -Gdpi=300 -Nfontname="Noto Sans Display" -Nfontsize=20 -Gfontname="Noto Sans Display" -Gfontsize=20 -Efontname="Noto Sans Display" -Efontsize=16 -o${OUT} $1
+dot -Tsvg -Gdpi=300 -Nfontname="Calibri" -Nfontsize=20 -Gfontname="Calibri" -Gfontsize=20 -Efontname="Calibri" -Efontsize=16 -o${OUT} $1
 inkscape --export-plain-svg --actions="export-id:graph0; export-id-only; vacuum-defs; export-filename:$2; export-plain-svg; export-text-to-path; export-do;" ${OUT} &>/dev/null
 rm -r ${OUT}
