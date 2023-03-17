d944815481ff6b932a36d0aa5b0bbc81dbed3a60
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-03-17 16:47:25 +0100

    build: create dir if not exist

--- scripts/generate-schemas.ts
+++ scripts/generate-schemas.ts
@@ -1,7 +1,13 @@
 import chalk from 'chalk'
-import { writeFileSync } from 'fs'
+import { mkdirSync, writeFileSync } from 'fs'
 import path from 'path'
 import { schemas } from '../docs/cloud-protocol/validateWithType'
+
+try {
+	mkdirSync(path.join(process.cwd(), 'build', 'html', 'docs', 'cloud-protocol'))
+} catch {
+	// pass
+}
 
 for (const schemaType of schemas) {
 	const schema = JSON.parse(JSON.stringify(schemaType))
