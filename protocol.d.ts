9a09f3923b8d6e2c81d60fcd5ebe30fac5348ebe
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-03-21 15:18:48 +0100

    feat: export data types

--- protocol.d.ts
+++ protocol.d.ts
@@ -1,3 +1,19 @@
+import { Static } from '@sinclair/typebox'
+import { AGPSRequest } from './docs/cloud-protocol/AGPSRequest'
+import { AWSDesired } from './docs/cloud-protocol/AWSDesired'
+import { AWSReported } from './docs/cloud-protocol/AWSReported'
+import { AzureDesired } from './docs/cloud-protocol/AzureDesired'
+import { AzureFOTA } from './docs/cloud-protocol/AzureFOTA'
+import { AzureReported } from './docs/cloud-protocol/AzureReported'
+import { Batch } from './docs/cloud-protocol/Batch'
+import { Config } from './docs/cloud-protocol/Config'
+import { Message } from './docs/cloud-protocol/Message'
+import { NeighboringCellMeasurements } from './docs/cloud-protocol/NeighboringCellMeasurements'
+import { NetworkSurvey } from './docs/cloud-protocol/NetworkSurvey'
+import { PGPSRequest } from './docs/cloud-protocol/PGPSRequest'
+import { PGPSResponse } from './docs/cloud-protocol/PGPSResponse'
+import { WiFiSiteSurvey } from './docs/cloud-protocol/WiFiSiteSurvey'
+
 export * from './docs/cloud-protocol/AGPSRequest'
 export * from './docs/cloud-protocol/AWSDesired'
 export * from './docs/cloud-protocol/AWSReported'
@@ -11,5 +27,22 @@ export * from './docs/cloud-protocol/NeighboringCellMeasurements'
 export * from './docs/cloud-protocol/NetworkSurvey'
 export * from './docs/cloud-protocol/PGPSRequest'
 export * from './docs/cloud-protocol/PGPSResponse'
-export * from './docs/cloud-protocol/validateWithType'
 export * from './docs/cloud-protocol/WiFiSiteSurvey'
+export * from './docs/cloud-protocol/validateWithType'
+
+export type AGPSRequestData = Static<typeof AGPSRequest>
+export type AWSDesiredData = Static<typeof AWSDesired>
+export type AWSReportedData = Static<typeof AWSReported>
+export type AzureDesiredData = Static<typeof AzureDesired>
+export type AzureFOTAData = Static<typeof AzureFOTA>
+export type AzureReportedData = Static<typeof AzureReported>
+export type BatchData = Static<typeof Batch>
+export type ConfigData = Static<typeof Config>
+export type MessageData = Static<typeof Message>
+export type NeighboringCellMeasurementsData = Static<
+	typeof NeighboringCellMeasurements
+>
+export type NetworkSurveyData = Static<typeof NetworkSurvey>
+export type PGPSRequestData = Static<typeof PGPSRequest>
+export type PGPSResponseData = Static<typeof PGPSResponse>
+export type WiFiSiteSurveyData = Static<typeof WiFiSiteSurvey>
