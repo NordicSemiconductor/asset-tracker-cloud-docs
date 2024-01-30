4129f989bcb2240e02fc2b9623492e888d65f056
Markus Tacker <Markus.Tacker@NordicSemi.no>
2024-01-30 15:58:25 +0100

    Revert "chore: update husky to v9"
    This reverts commit 004dbeeab5e3187539967dc41df5f8ad4155761f.

--- docs/cloud-protocol/$id.ts
+++
@@ -1,4 +0,0 @@
-export const $id = (id: string): string =>
-	`https://nordicsemiconductor.github.io/asset-tracker-cloud-docs/${
-		process.env.VERSION ?? 'saga'
-	}/docs/cloud-protocol/${id}.schema.json`
--- sphinx_ncs_theme/README.md
+++
@@ -1,3 +0,0 @@
-This is based on
-[version 1.0.19](https://pypi.org/project/sphinx-ncs-theme/1.0.19/) of the
-sphinx-ncs-theme ([source](https://github.com/nrfconnect/doc-sphinx-ncs-theme)).
